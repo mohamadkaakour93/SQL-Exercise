340569f18ccbf3471900a2c411ffe284da19df76
mohamadelkaakour <mhamadelkaakour@gmail.com>
2021-06-03 12:25:44 +0300

    done

--- answers.sh
+++ answers.sh
@@ -2,7 +2,7 @@
 2) Select * FROM students WHERE age > 30
 3) SELECT name FROM students WHERE age = 30 AND Gender = "F" 
 4) SELECT Points FROM students WHERE name ="Alex" 
-5) INSERT INTO students
+5) INSERT INTO students(name,Age,Gender,Points)
 VALUES ('Mohamad','27','M',500); 
 6) UPDATE students SET Points= 530 WHERE ID= 2 
 7) UPDATE students SET Points= 190 WHERE ID= 1 
