98746d759e9d9d1b88c119745fc2c1897c4f4c22
mohamadelkaakour <mhamadelkaakour@gmail.com>
2021-06-04 09:32:53 +0300

    Done

--- answers.sh
+++ answers.sh
@@ -4,18 +4,18 @@
 4) SELECT Points FROM students WHERE name ="Alex" 
 5) INSERT INTO students(name,Age,Gender,Points)
 VALUES ('Mohamad','27','M',500); 
-6) UPDATE students SET Points= 530 WHERE ID= 2 
-7) UPDATE students SET Points= 190 WHERE ID= 1 
+6) UPDATE students SET Points= 530 WHERE name = "Basma"
+7) UPDATE students SET Points= 190 WHERE name = "Alex"
 
 #creating table 
 
-1) INSERT INTO  graduates (Id,Name,Age,Gender,Points)
-SELECT Id,Name,Age,Gender,Points FROM students
-WHERE ID = 4;
+1) INSERT INTO  graduates (Name,Age,Gender,Points)
+SELECT Name,Age,Gender,Points FROM students
+WHERE Name = "Layal";
 
-2) UPDATE graduates SET Graduation = '08/09/2018' WHERE id = 4
+2) UPDATE graduates SET Graduation = '08/09/2018' WHERE name = "Layal"
 
-3) DELETE FROM students WHERE ID = 4;
+3) DELETE FROM students WHERE name = "Layal"
 
 #join 
 1) SELECT employees.name, employees.Company, companies.Date
@@ -32,7 +32,11 @@ INNER JOIN companies ON employees.Company = companies.name )
 WHERE employees.Role='Graphic Designer';
 
 #count and filter 
-1) SELECT Name,MAX(Points) FROM students;
+1) SELECT name, points
+FROM students
+GROUP by name
+HAVING points = (select max(points) from students);
+
 2) SELECT AVG(Points) FROM students;
 3) SELECT count(*) FROM students WHERE Points=500;
 4) SELECT name FROM students WHERE name like "%s%"
